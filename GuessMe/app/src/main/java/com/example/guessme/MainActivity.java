8d1835065f3ab01df8b1edff0820611e46363a8d
sai Pavan Kumar <gandhipavan555@gmail.com>
2019-06-07 11:27:23 +0530

    generated random number after success

--- GuessMe/app/src/main/java/com/example/guessme/MainActivity.java
+++ GuessMe/app/src/main/java/com/example/guessme/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.guessme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -13,7 +12,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int randomNumber = getRandomNumber(100);
+    int randomNumber = getRandomNumber(100);
 
     int noOfGuesses = 0;
     @Override
@@ -31,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
         String message;
 
         if (guessNumber == randomNumber) {
-            message ="you Guessed it right";
+            message ="you got it! Try again !!";
             countTv.setText(String.valueOf(noOfGuesses+1));
+            randomNumber = getRandomNumber(100);
             noOfGuesses=0;
         } else {
             if (guessNumber > randomNumber) {
