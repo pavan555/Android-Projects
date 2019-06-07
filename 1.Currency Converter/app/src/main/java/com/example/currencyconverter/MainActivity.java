755f30c951427166f0ed09b7791d9af6a077a06f
sai Pavan Kumar <gandhipavan555@gmail.com>
2019-06-07 20:57:44 +0530

    fixed crashing issue

--- 1.Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ 1.Currency Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,18 +10,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     final Double exchangeValue = 69.38;
-    public void changeCurrency(View view){
+    public void changeCurrency(View view) {
         EditText dollarTv = findViewById(R.id.dollarTv);
 
-        String rupee = getResources().getString(R.string.rupee);
-        String dollar = getResources().getString(R.string.dollar);
-        TextView rupeeTv = findViewById(R.id.rupeeTv);
-        Double rupees = Double.parseDouble(dollarTv.getText().toString()) * exchangeValue;
-        String rupeesValue = String.format("%.2f",rupees);
-        rupeeTv.setText(String.format("%s%s Will be %s%s",dollar,dollarTv.getText().toString(), rupee, rupeesValue));
+        if (dollarTv.getText().toString().isEmpty()) {
+            Toast.makeText(this, "please enter amount", Toast.LENGTH_SHORT).show();
+        } else {
+            String rupee = getResources().getString(R.string.rupee);
+            String dollar = getResources().getString(R.string.dollar);
+            TextView rupeeTv = findViewById(R.id.rupeeTv);
+            Double rupees = Double.parseDouble(dollarTv.getText().toString()) * exchangeValue;
+            String rupeesValue = String.format("%.2f", rupees);
+            rupeeTv.setText(String.format("%s%s Will be %s%s", dollar, dollarTv.getText().toString(), rupee, rupeesValue));
 //        Toast.makeText(this,rupee+dollarTv.getText().toString(),Toast.LENGTH_SHORT).show();
-        rupeeTv.setVisibility(View.VISIBLE);
+            rupeeTv.setVisibility(View.VISIBLE);
 
+        }
     }
 
     @Override
